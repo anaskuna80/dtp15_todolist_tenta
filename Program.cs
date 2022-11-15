eb3264b39de7316d158b6e37eb131dbbef8eb275
Andreas <andreas.kuylenstierna@gmail.com>
2022-11-15 19:11:30 +0100

    Added some comments...

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
 ﻿namespace dtp15_todolist
 {
-    public class Todo // Klass ToDo - Här defineras allting...
+    public class Todo // Klass ToDo - Definering av status per uppgift
     {
-        public static List<TodoItem> list = new List<TodoItem>();
+        public static List<TodoItem> list = new List<TodoItem>(); // Skapande av listan list.
 
-        public const int Active = 1;
-        public const int Waiting = 2;
-        public const int Ready = 3;
+        public const int Active = 1; // Konstanten Active, aktiv
+        public const int Waiting = 2; // Konstanten Waiting, väntande
+        public const int Ready = 3; // Konstanten Ready, avklarad
         public static string StatusToString(int status)
         {
-            switch (status)
+            switch (status) // Tilldelande av ekvivalenter för repektive status 
             {
                 case Active: return "aktiv";
                 case Waiting: return "väntande";
@@ -17,64 +17,64 @@
                 default: return "(felaktig)";
             }
         }
-        public class TodoItem
+        public class TodoItem // Klass för att dela upp varje rad i [filnamn] i 4 delar med hjälp av "|".
         {
-            public int status;
+            public int status; // Först initiering av två int (status och priority) och två strängar (task och taskDesctiption)
             public int priority;
             public string task;
             public string taskDescription;
-            public TodoItem(int priority, string task)
+            public TodoItem(int priority, string task) // Lista alla aktiva poster
             {
                 this.status = Active;
                 this.priority = priority;
                 this.task = task;
                 this.taskDescription = "";
             }
-            public TodoItem(string todoLine)
+            public TodoItem(string todoLine) // Skapande av array för uppdelning i fyra "fält"
             {
-                string[] field = todoLine.Split('|');
-                status = Int32.Parse(field[0]);
-                priority = Int32.Parse(field[1]);
-                task = field[2];
-                taskDescription = field[3];
+                string[] field = todoLine.Split('|'); // Dela allt i array/strängen "field" vid varje pipe
+                status = Int32.Parse(field[0]); // status har index 0
+                priority = Int32.Parse(field[1]); // priority har index 1
+                task = field[2]; // task har index 2
+                taskDescription = field[3]; // taskDescription har index 3 av max 4 (0-3)
             }
-            public void Print(bool verbose = false)
+            public void Print(bool verbose = false) // Metoden Print som har variabeln verbose(=mångordig) satt till false som default.
             {
                 string statusString = StatusToString(status);
-                Console.Write($"|{statusString,-12}|{priority,-6}|{task,-20}|");
-                if (verbose)
-                    Console.WriteLine($"{taskDescription,-40}|");
+                Console.Write($"|{statusString,-12}|{priority,-6}|{task,-25}|"); // utskrift av task
+                if (verbose) // om verbose är sann så...
+                    Console.WriteLine($"{taskDescription,-40}|"); //...skriv ut hela uppgiftens beskriving.
                 else
-                    Console.WriteLine();
+                    Console.WriteLine(); // eller skriv en tom rad.
             }
         }
-        public static void ReadListFromFile()
+        public static void ReadListFromFile() // Metod för inläsning av fil användande av StreamReader.
         {
-            string todoFileName = "todo.lis";
-            Console.Write($"Läser från fil {todoFileName} ... ");
-            StreamReader sr = new StreamReader(todoFileName);
+            string todoFileName = "todo.lis"; // Hårdkodning av att använda filen todo.lis -- TBD
+            Console.Write($"Läser från fil {todoFileName} ... "); // Talar om för användaren att filen är inläst.
+            StreamReader sr = new StreamReader(todoFileName); // Men det är inte gjort förrän här.
             int numRead = 0;
 
             string line;
-            while ((line = sr.ReadLine()) != null)
+            while ((line = sr.ReadLine()) != null) // Sålänge inläsning sr inte är null så...
             {
-                TodoItem item = new TodoItem(line);
-                list.Add(item);
+                TodoItem item = new TodoItem(line); // ...skapa en ny rad av inläst fil
+                list.Add(item); // Lägg till detta i listan list.
                 numRead++;
             }
-            sr.Close();
-            Console.WriteLine($"Läste {numRead} rader.");
+            sr.Close(); // Stäng fil
+            Console.WriteLine($"Läste {numRead} rader."); // Talar om hur många rader filen bestod av.
         }
-        private static void PrintHeadOrFoot(bool head, bool verbose)
+        private static void PrintHeadOrFoot(bool head, bool verbose) // Nu följer tre privata metoder för utskrift av data från inläst fil.
         {
             if (head)
             {
-                Console.Write("|status      |prio  |namn                |");
+                Console.Write("|status      |prio  |namn                     |");
                 if (verbose) Console.WriteLine("beskrivning                             |");
                 else Console.WriteLine();
             }
-            Console.Write("|------------|------|--------------------|");
-            if (verbose) Console.WriteLine("----------------------------------------|");
+            Console.Write("|------------|------|-------------------------|");
+            if (verbose) Console.WriteLine("---------------------------------------------|");
             else Console.WriteLine();
         }
         private static void PrintHead(bool verbose)
