1a8e8cbb1b2af793b438adb5cb7586890e6ddc14
Andreas <andreas.kuylenstierna@gmail.com>
2022-11-16 17:06:44 +0100

    Gjorde klart hjälp...

--- Program.cs
+++ Program.cs
@@ -54,12 +54,12 @@
             public int priority;
             public string task;
             public string taskDescription;
-            public TodoItem(int priority, string task) // Konstruktor för skapande av nya poster
+            public TodoItem(int priority, string task, string taskInfo) // Konstruktor för skapande av nya poster
             {
                 this.status = Active;
                 this.priority = priority;
                 this.task = task;
-                this.taskDescription = "";
+                this.taskDescription = taskInfo;
             }
             public TodoItem(string todoLine) // Konstruktor för skapande av array för uppdelning i fyra "fält" av inläst fil.
             {
@@ -82,7 +82,16 @@
         public static void ReadListFromFile() // Metod för inläsning av fil användande av StreamReader.
         {
             string todoFileName = "todo.lis"; // Hårdkodning av att använda filen todo.lis -- TBD
-            Console.Write($"Läser från fil {todoFileName} ... "); // Talar om för användaren att filen är inläst.
+            Console.Write($" Läser från fil "); 
+            Console.BackgroundColor = ConsoleColor.DarkRed; 
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(todoFileName); // Talar om för användaren att filen är inläst.
+            Console.ResetColor();
+            Console.Write("."); Thread.Sleep(500);
+            Console.Write("."); Thread.Sleep(500); 
+            Console.Write("."); Thread.Sleep(500);
+            Console.Write("."); Thread.Sleep(500);
+            Console.Write("."); Thread.Sleep(500);
             StreamReader sr = new StreamReader(todoFileName); // Men det är inte gjort förrän här.
             int numRead = 0;
 
@@ -94,7 +103,7 @@
                 numRead++;
             }
             sr.Close(); // Stäng fil
-            Console.WriteLine($"Läste {numRead} rader."); // Talar om hur många rader filen bestod av.
+            Console.WriteLine($" Läste {numRead} rader.\n"); // Talar om hur många rader filen bestod av.
         }
         // Nu följer tre privata metoder för utskrift av data från inläst fil:
         private static void PrintHeadOrFoot(bool head, bool verbose) // Första privata metoden initierar printhuvud och fot för mall till utskrift.
@@ -128,17 +137,26 @@
         }
         public static void PrintHelp() // Hjälplista NYI -- Lägg till flera valmöjligheter
         {
-            Console.WriteLine("Kommandon:");
-            Console.WriteLine("hjälp    lista denna hjälp");
-            Console.WriteLine("lista    lista att-göra-listan");
-            Console.WriteLine("sluta    spara att-göra-listan och sluta");
+            Console.WriteLine(" +----------------------------------------------------------------------------+");
+            Console.WriteLine(" | Kommandon:                                                                 |\n +----------------------------------------------------------------------------+");
+            Console.WriteLine(" |       \"hjälp\"        :  Visa denna hjälp.                            YEAH! |");
+            Console.WriteLine(" |       \"lista\"        :  Lista att-göra-listan.                         OK! |");
+            Console.WriteLine(" |     \"lista allt\"     :  Lista allt aktivt i att-göra-listan.          TBD! |");
+            Console.WriteLine(" |       \"beskriv\"      :  Lista precis allt i att-göra-listan.          NYI! |");
+            Console.WriteLine(" |         \"ny\"         :  Skapa ny uppgift i att-göra-listan.           NYI! |");
+            Console.WriteLine(" | \"aktivera /uppgift/\" :  Göra vald uppgift aktiv i att-göra-listan.    NYI! |");
+            Console.WriteLine(" |     \"klar /uppgift/\" :  Göra vald uppgift avklarad i att-göra-listan. NYI! |");
+            Console.WriteLine(" |    \"vänta /uppgift/\" :  Göra vald uppgift väntande i att-göra-listan. NYI! |");
+            Console.WriteLine(" |        \"sluta\"       :  Spara att-göra-listan och avsluta programmet. TBD! |");
+            Console.WriteLine(" +----------------------------------------------------------------------------+");
         }
     }
     class MainClass
     {
         public static void Main(string[] args) // Huvudprogram.
         {
-            Console.WriteLine("Välkommen till att-göra-listan!"); // Hälsar användaren välkommen.
+            Console.WriteLine(" Välkommen till att-göra-listan!"); // Hälsar användaren välkommen.
+            Console.WriteLine(" -------------------------------\n");
             Todo.ReadListFromFile(); // Inläsning av fil, kallar på den funktionen i klassen Todo.
             Todo.PrintHelp(); // Utskrift av meny/hjälp, kallar på den funktionen i klassen Todo.
             string command; // initiering av en sträng som "behållare" för användarens inmatning
@@ -154,6 +172,37 @@
                     Console.WriteLine("Hej då!");
                     break;
                 }
+                else if (MyIO.Equals(command, "ny"))
+                {
+                    int status = 1;
+                    string nyprio, nytask, nyinfo;
+                    List<string> newtask = new List<string>();
+                    Console.Write("\n Vad är det för uppgift du vill lägga till? -> ");
+                    nytask = Console.ReadLine();
+                    if (nytask.Length <= 2) { Console.WriteLine($"{nytask} tillagd"); }
+                    else { Console.WriteLine("Uppgift för kort!"); }
+                    Console.WriteLine("Kan ge en kort beskrivning av uppgiften? -> ");
+                    nyinfo = Console.ReadLine();
+                    Console.Write($"Vilket prio vill du sätta på {nytask}? (Ange 1-3) >");
+                    nyprio = Console.ReadLine();
+                    if (nyprio == "0" || nyprio == "4") { Console.WriteLine("Denna prio finns inte!"); }
+                    else if (nyprio == "1") { newtask.Add(nyprio); }
+                    else if (nyprio == "2") { newtask.Add(nyprio); }
+                    else if (nyprio == "3") { newtask.Add(nyprio); }
+                    else throw new OutOfMemoryException();
+                    newtask.Add(nytask);
+                    newtask.Add(nyinfo);
+                    Console.WriteLine(newtask);
+                    break;
+                }
+                else if (MyIO.Equals(command, "exit") || MyIO.Equals(command, "quit") || MyIO.Equals(command, "avsluta"))
+                {
+                    Console.WriteLine("För att avsluta detta lilla program skriv \"sluta\"");
+                }
+                else if (MyIO.Equals(command, "authors") || MyIO.Equals(command, "programmerad") || MyIO.Equals(command, "cred"))
+                {
+                    Console.WriteLine("Programmet är skriven av TomKi the teacher och Andreas Kuylenstierna");
+                }
                 else if (MyIO.Equals(command, "lista")) // Annars om command är lista så... en ny if sats startar:
                 {
                     if (MyIO.HasArgument(command, "allt")) // Om command innehåller "lista" + allt så ska beskrivning inkluderas i utskrift
@@ -163,11 +212,10 @@
                 }
                 else // Annars meddelas användaren att han/hon har skrivit in ett felaktigt kommando.
                 {
-                    Console.WriteLine($"Okänt kommando: {command}"); 
+                    Console.WriteLine($"Okänt/ogiltigt kommando: \"{command}\"");
                 }
             }
             while (true); // Kör loopen medans sant
         }
-    }
-   
+    }  
 }
