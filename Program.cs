cd7bcc1c2c8e0ce9b07f2c72e8a0ae8e2e92c3d4
Andreas <andreas.kuylenstierna@gmail.com>
2022-11-16 11:13:58 +0100

    Mera kommentarer tillagda...

--- Program.cs
+++ Program.cs
@@ -23,14 +23,14 @@
             public int priority;
             public string task;
             public string taskDescription;
-            public TodoItem(int priority, string task) // Lista alla aktiva poster
+            public TodoItem(int priority, string task) // Konstruktor för skapande av nya poster
             {
                 this.status = Active;
                 this.priority = priority;
                 this.task = task;
                 this.taskDescription = "";
             }
-            public TodoItem(string todoLine) // Skapande av array för uppdelning i fyra "fält"
+            public TodoItem(string todoLine) // Konstruktor för skapande av array för uppdelning i fyra "fält" av inläst fil.
             {
                 string[] field = todoLine.Split('|'); // Dela allt i array/strängen "field" vid varje pipe
                 status = Int32.Parse(field[0]); // status har index 0
@@ -65,7 +65,8 @@
             sr.Close(); // Stäng fil
             Console.WriteLine($"Läste {numRead} rader."); // Talar om hur många rader filen bestod av.
         }
-        private static void PrintHeadOrFoot(bool head, bool verbose) // Nu följer tre privata metoder för utskrift av data från inläst fil.
+        // Nu följer tre privata metoder för utskrift av data från inläst fil:
+        private static void PrintHeadOrFoot(bool head, bool verbose) // Första privata metoden initierar printhuvud och fot för mall till utskrift.
         {
             if (head)
             {
@@ -77,15 +78,15 @@
             if (verbose) Console.WriteLine("---------------------------------------------|");
             else Console.WriteLine();
         }
-        private static void PrintHead(bool verbose)
+        private static void PrintHead(bool verbose) // Metod två kallar på ovanstående metod och sätter head som sann
         {
             PrintHeadOrFoot(head: true, verbose);
         }
-        private static void PrintFoot(bool verbose)
+        private static void PrintFoot(bool verbose) // Metod tre kallar på metod 1 för att sätta head till false
         {
             PrintHeadOrFoot(head: false, verbose);
         }
-        public static void PrintTodoList(bool verbose = false)
+        public static void PrintTodoList(bool verbose = false) // utskrift av varje rad med en foreach utan description
         {
             PrintHead(verbose);
             foreach (TodoItem item in list)
@@ -94,7 +95,7 @@
             }
             PrintFoot(verbose);
         }
-        public static void PrintHelp()
+        public static void PrintHelp() // Hjälplista NYI -- Lägg till flera valmöjligheter
         {
             Console.WriteLine("Kommandon:");
             Console.WriteLine("hjälp    lista denna hjälp");
@@ -104,66 +105,66 @@
     }
     class MainClass
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) // Huvudprogram.
         {
-            Console.WriteLine("Välkommen till att-göra-listan!");
-            Todo.ReadListFromFile();
-            Todo.PrintHelp();
-            string command;
-            do
+            Console.WriteLine("Välkommen till att-göra-listan!"); // Hälsar användaren välkommen.
+            Todo.ReadListFromFile(); // Inläsning av fil, kallar på den funktionen i klassen Todo.
+            Todo.PrintHelp(); // Utskrift av meny/hjälp, kallar på den funktionen i klassen Todo.
+            string command; // initiering av en sträng som "behållare" för användarens inmatning
+            do // Startar Loop
             {
-                command = MyIO.ReadCommand("> ");
-                if (MyIO.Equals(command, "hjälp"))
+                command = MyIO.ReadCommand("> "); // Kallar på metoden ReadCommand i klassen MyIO för att be användaren om inmatning.
+                if (MyIO.Equals(command, "hjälp")) // Start av If sats... Om command är hjälp så skriv ut menyn en gång till. NYI... Fler else if.
                 {
-                    Todo.PrintHelp();
+                    Todo.PrintHelp(); // Utskrift av meny/hjälp.
                 }
-                else if (MyIO.Equals(command, "sluta"))
+                else if (MyIO.Equals(command, "sluta")) // Annars om command är sluta så skriv ut ett hejdå dör att hoppa ur loop.
                 {
                     Console.WriteLine("Hej då!");
                     break;
                 }
-                else if (MyIO.Equals(command, "lista"))
+                else if (MyIO.Equals(command, "lista")) // Annars om command är lista så... en ny if sats startar:
                 {
-                    if (MyIO.HasArgument(command, "allt"))
+                    if (MyIO.HasArgument(command, "allt")) // Om command innehåller "lista" + allt så ska beskrivning inkluderas i utskrift
                         Todo.PrintTodoList(verbose: true);
                     else
-                        Todo.PrintTodoList(verbose: false);
+                        Todo.PrintTodoList(verbose: false); // Annars ska inte beskrivning bli utskriven men bara det nödvändigaste.
                 }
-                else
+                else // Annars meddelas användaren att han/hon har skrivit in ett felaktigt kommando.
                 {
-                    Console.WriteLine($"Okänt kommando: {command}");
+                    Console.WriteLine($"Okänt kommando: {command}"); 
                 }
             }
-            while (true);
+            while (true); // Kör loopen medans sant
         }
     }
-    class MyIO
+    class MyIO // Initiering av klassen MyIO
     {
-        static public string ReadCommand(string prompt)
+        static public string ReadCommand(string prompt) // Inläsning av kommandon
         {
             Console.Write(prompt);
-            return Console.ReadLine();
+            return Console.ReadLine(); // Här sker inläsning
         }
-        static public bool Equals(string rawCommand, string expected)
+        static public bool Equals(string rawCommand, string expected) // Metoden Equals jämför inmatningen och kollar om det är initierat i programmet
         {
-            string command = rawCommand.Trim();
-            if (command == "") return false;
+            string command = rawCommand.Trim(); // Städa upp kommando
+            if (command == "") return false; // Om commando är tom så returnera false
             else
             {
-                string[] cwords = command.Split(' ');
-                if (cwords[0] == expected) return true;
+                string[] cwords = command.Split(' '); // Annars dela upp strängen av det inmatna kommandot med mellanslag
+                if (cwords[0] == expected) return true; // Om första kommandot är det samma som väntas returnera true
             }
-            return false;
+            return false; // ... annars false
         }
-        static public bool HasArgument(string rawCommand, string expected)
+        static public bool HasArgument(string rawCommand, string expected) // Denna metod kollar om det finns fler argument än ett i inmatning.
         {
-            string command = rawCommand.Trim();
+            string command = rawCommand.Trim(); // Ta bort onödiga mellanslag
             if (command == "") return false;
             else
             {
-                string[] cwords = command.Split(' ');
-                if (cwords.Length < 2) return false;
-                if (cwords[1] == expected) return true;
+                string[] cwords = command.Split(' '); // Dela upp inmatna sträng till en array med mellanslag
+                if (cwords.Length < 2) return false; // Om det bara blir ett ord av inmatningen så returnera false
+                if (cwords[1] == expected) return true; // Annars sant
             }
             return false;
         }
